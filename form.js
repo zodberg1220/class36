d4a31ed6688fc34c2b38bc8ac22ec543adfd0f99
zodberg1220 <72825691+zodberg1220@users.noreply.github.com>
2021-02-03 14:12:17 -0800

    Add files via upload

---
+++ form.js
@@ -0,0 +1,38 @@
+class Form{
+constructor(){
+
+}
+
+display(){
+var title=createElement('h2');
+title.html("car racing game");
+title.position(130,0);
+
+var input=createInput("name");
+var button=createButton("play");
+var greeting=createElement("h3");
+
+
+input.position(130,160);
+button.position(250,200);
+
+button.mousePressed(function(){
+    input.hide();
+    button.hide();
+    var name=input.value();
+    player.update(name);
+    playerCount+=1;
+    player.updateCount(playerCount);
+    
+    greeting.html("hello "+name);
+    greeting.position(130,160);
+})
+
+
+
+}
+
+
+
+
+}
